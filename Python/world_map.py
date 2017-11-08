346dc9b8b533685c4f3ffd71c1d22cdaff742d1a
Wewarwo <60070042@kmitl.ac.th>
2017-11-08 18:45:55 +0700

    add style

--- Python/world_map.py
+++ Python/world_map.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pygal as pg
 import math
-from pygal.style import Style
 import numpy as np
 #############################################################################################################################################################################################################################
 def column_index(df, query_cols):
@@ -34,19 +33,13 @@ def main():
             print(idx)
             reg[val[1]] += df_world.iloc[idx, col] if not math.isnan(df_world.iloc[idx, col]) else 0
 #############################################################################################################################################################################################################################
-    custom_style = Style(
-                    background='Black',
-                    plot_background='Black',
-                    foreground='White',
-                    foreground_strong='White',
-                    foreground_subtle='#630C0D',
-                    opacity='.6',
-                    transition='100ms ease-in',
-                    colors=('#E853A0',  '#E8537A')
-                    )
+    from pygal.style import NeonStyle
+    NeonStyle = NeonStyle(
+            colors=('#FF6B89', '#FF0000')
+            )
 #############################################################################################################################################################################################################################
-    worldmap_chart = pg.maps.world.World()
-    worldmap_chart.title = 'Leo World'
+    worldmap_chart = pg.maps.world.World(fill=True, interpolate='cubic', style=NeonStyle)
+    worldmap_chart.title = 'World Populations'
     worldmap_chart.add('In {}'.format(year), reg)
     worldmap_chart.render_to_file('Graph SVG/World_Map.svg') 
 main()
