3e79f3c0b57874efae63bab32e02f61c9ce45f79
wewarwo <60070042@kmitl.ac.th>
2017-11-08 16:54:38 +0700

    gender&pop refac

--- Python/PopEachCountry.py
+++ Python/PopEachCountry.py
@@ -1,31 +1,33 @@
 """PopEachContry"""
 import pandas as pd
 import pygal
-total_csv = pd.read_csv("../DATA/Total population.csv", encoding = "UTF-8")
-country = input()
-years1 = int(input())
-years2 = int(input())
-pop_list = list()
-years_list = list()
-hav = 0
+def worldpop():
+    total_csv = pd.read_csv("../DATA/Total population.csv", encoding = "UTF-8")
+    country = input()
+    years1 = int(input())
+    years2 = int(input())
+    pop_list = list()
+    years_list = list()
+    hav = False
 
-for name in range(len(total_csv['Country Name'])):
-    if total_csv['Country Name'][name] == country:
-        hav = 1
-        for year in range(years1, years2+1):
-            pop_list.append(int(total_csv[str(year)][name]))
+    for name in range(len(total_csv['Country Name'])):
+        if total_csv['Country Name'][name] == country:
+            hav = True
+            for year in range(years1, years2+1):
+                pop_list.append(int(total_csv[str(year)][name]))
 
-from pygal.style import NeonStyle
-NeonStyle = NeonStyle(
-		colors=('#0000FF', '#FF0000')
-		)
-if hav == 1:
+    from pygal.style import NeonStyle
+    NeonStyle = NeonStyle(
+    		colors=('#0000FF', '#FF0000')
+    		)
+    if hav:
 
-    line_chart = pygal.Line(fill=True, interpolate='cubic', style=NeonStyle)
-    line_chart.title = country
-    line_chart.x_labels = range(years1, years2+1)
-    line_chart.add('Populaions', pop_list)
-    line_chart.render_to_file('Graph SVG/pop_bar_chart.svg')  
-    print(*pop_list)
-else:
-    print('Not Found')
+        line_chart = pygal.Line(fill=True, interpolate='cubic', style=NeonStyle)
+        line_chart.title = country
+        line_chart.x_labels = range(years1, years2+1)
+        line_chart.add('Populaions', pop_list)
+        line_chart.render_to_file('Graph SVG/pop_bar_chart.svg')  
+        print(*pop_list)
+    else:
+        print('not Found')
+worldpop()
--- Python/genderper.py
+++ Python/genderper.py
@@ -1,35 +1,36 @@
 """%male&female"""
 import pandas as pd
 import pygal
+def malefemale():
+    total_male = pd.read_csv("../DATA/Male/Male Total Population.csv", encoding = "UTF-8")
+    total_female = pd.read_csv("../DATA/Female/Female Total Population.csv", encoding = "UTF-8")
+    country = input()
+    year = input()
+    hav = False
 
-total_male = pd.read_csv("../DATA/Male/Male Total Population.csv", encoding = "UTF-8")
-total_female = pd.read_csv("../DATA/Female/Female Total Population.csv", encoding = "UTF-8")
-country = input()
-year = input()
-hav = 0
+    for i in range(len(total_male['Country Name'])):
+        if total_male['Country Name'][i] == country:
+            male_per = (total_male[year][i]*100)/(total_male[year][i]+total_female[year][i])
+            female_per = (total_female[year][i]*100)/(total_male[year][i]+total_female[year][i])
+            male = int(total_male[year][i])
+            female = int(total_female[year][i])
+            hav = True
+            break
+    if hav:
+    	print('Males :', male)
+    	print('Females :', female)
+    	print('Totals :', male+female)
 
-for i in range(len(total_male['Country Name'])):
-    if total_male['Country Name'][i] == country:
-        male_per = (total_male[year][i]*100)/(total_male[year][i]+total_female[year][i])
-        female_per = (total_female[year][i]*100)/(total_male[year][i]+total_female[year][i])
-        male = int(total_male[year][i])
-        female = int(total_female[year][i])
-        hav = 1
-        break
-if hav == 1:
-	print('Males :', male)
-	print('Females :', female)
-	print('Totals :', male+female)
+    	from pygal.style import NeonStyle
+    	NeonStyle = NeonStyle(
+    		colors=('#0000FF', '#FF0000')
+    		)
+    	pie_chart = pygal.Pie(fill=True, interpolate='cubic', style=NeonStyle)
 
-	from pygal.style import NeonStyle
-	NeonStyle = NeonStyle(
-		colors=('#0000FF', '#FF0000')
-		)
-	pie_chart = pygal.Pie(fill=True, interpolate='cubic', style=NeonStyle)
-
-	pie_chart.title = 'Males and Females (in %)'
-	pie_chart.add('Males : {}'.format(male), male_per)
-	pie_chart.add('Females : {}'.format(female), female_per)
-	pie_chart.render_to_file('Graph SVG/gender_pie_chart.svg')
-else:
-	print('not found')
+    	pie_chart.title = 'Males and Females (in %)'
+    	pie_chart.add('Males : {}'.format(male), male_per)
+    	pie_chart.add('Females : {}'.format(female), female_per)
+    	pie_chart.render_to_file('Graph SVG/gender_pie_chart.svg')
+    else:
+    	print('not found')
+malefemale()
